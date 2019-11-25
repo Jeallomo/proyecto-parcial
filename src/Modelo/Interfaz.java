3c0d564c8d2556b9603577ea865e836455c8f0a3
JesusLozada <jeallomo@hotmail.com>
2019-11-25 18:40:06 -0500

    Agregue el JFrame de la interfaz y su metodo para hacerlo visible

--- src/Modelo/Interfaz.java
+++ src/Modelo/Interfaz.java
@@ -1,5 +1,19 @@
 package Modelo;
 
-public class Interfaz {
+import javax.swing.JFrame;
 
+public class Interfaz {
+	private JFrame ventana;
+	
+	Interfaz(){
+		ventana = new JFrame();
+		ventana.setBounds(15, 15, 600, 600);
+		ventana.setTitle("Ajedrez");
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.getContentPane().setLayout(null);
+	}
+	
+	public void mostrar() {
+		ventana.setVisible(true);
+	}	
 }
