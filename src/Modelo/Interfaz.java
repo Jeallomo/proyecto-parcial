cc69cce4c6769c78ac2a49ee2486a796ef84c0d1
JesusLozada <jeallomo@hotmail.com>
2019-11-25 19:48:02 -0500

    Agregando los botones que formaran el tablero

--- src/Modelo/Interfaz.java
+++ src/Modelo/Interfaz.java
@@ -1,16 +1,49 @@
 package Modelo;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Interfaz {
+	//Arreglos
+	private JButton[][] botonesCasillas= new JButton[8][8];
+	
+	//Componentes Graficos
 	private JFrame ventana;
+	private JPanel tablero;
 	
 	Interfaz(){
+		int i,j;
+		
 		ventana = new JFrame();
-		ventana.setBounds(15, 15, 600, 600);
 		ventana.setTitle("Ajedrez");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		ventana.getContentPane().setLayout(null);
+		
+		tablero = new JPanel();
+		tablero.setPreferredSize(new Dimension(470,470));
+		tablero.setLayout(null);
+		
+		for(i=0;i<8;i++) {
+			for(j=0;j<8;j++) {
+				botonesCasillas[i][j] = new JButton();
+				botonesCasillas[i][j].setBounds(i*60, j*60, 60, 60);
+				
+				if((i+j)%2 == 0) {
+					botonesCasillas[i][j].setBackground(new Color(255,255,255));
+				} else {
+					botonesCasillas[i][j].setBackground(new Color(0,0,0));
+				}
+				
+				tablero.add(botonesCasillas[i][j]);	
+			}
+		}
+		
+		ventana.getContentPane().add(tablero);
+		ventana.pack();
+		ventana.setResizable(false);
 	}
 	
 	public void mostrar() {
--- src/Modelo/MainClass.java
+++ src/Modelo/MainClass.java
@@ -2,4 +2,9 @@ package Modelo;
 
 public class MainClass {
 
+	public static void main(String[] args) {
+		Interfaz ajedrez = new Interfaz();
+		
+		ajedrez.mostrar();
+	}
 }
